b7c290d212ff914c1fe36abe3f085b55a17f914f
Casey Foster <c@sey.me>
2017-08-26 23:40:00 -0500

    Simplify client cleanup

--- src/accessories/harmony/get-client.js
+++ src/accessories/harmony/get-client.js
@@ -2,7 +2,6 @@ const _ = require('underscore');
 const {accessories, port = 10000} = require('../../../config');
 const Discover = require('harmonyhubjs-discover');
 const getClient = require('harmonyhubjs-client');
-const log = require('../../utils/log');
 
 let discover;
 
@@ -40,25 +39,12 @@ module.exports = async friendlyName => {
   let client = clients[ip];
   if (client) return client;
 
-  const reset = _.once(() => delete clients[ip]);
-
   try {
-    let online = false;
     client = await (clients[ip] = getClient(ip));
-    client._xmppClient
-      .on('online', () => online = true)
-      .on('offline', () => {
-        online = false;
-        reset();
-      })
-      .on('error', er => {
-        log.error(er);
-        reset();
-        if (online) client.end();
-      });
+    client._xmppClient.on('offline', () => delete clients[ip]);
     return client;
   } catch (er) {
-    reset();
+    delete clients[ip];
     throw er;
   }
 };
